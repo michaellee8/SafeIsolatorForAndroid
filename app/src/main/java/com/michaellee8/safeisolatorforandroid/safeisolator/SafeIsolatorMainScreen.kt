164f196e9965cf32068eb67622847b2bae19eb2a
michaellee8 <ckmichael8@gmail.com>
2022-04-10 01:44:40 +0800

    feat: design better ui

--- app/src/main/java/com/michaellee8/safeisolatorforandroid/safeisolator/SafeIsolatorMainScreen.kt
+++ app/src/main/java/com/michaellee8/safeisolatorforandroid/safeisolator/SafeIsolatorMainScreen.kt
@@ -16,13 +16,35 @@ fun SafeIsolatorMainScreen(
     vpnEnabled: Boolean,
 ) {
     Column {
-        if (vpnEnabled) {
-            Button(onClick = { disableVpn() }) {
-                Text("Disable VPN")
+        if (isInstalledOnWorkProfile) {
+            Text(text = "Running in Work Profile.")
+            Text(text = "1. Setup VPN.")
+            if (vpnEnabled) {
+                Text(text = "VPN is already running, you are protected!")
             }
         } else {
-            Button(onClick = { enableVpn() }) {
-                Text("Enable VPN")
+            Text(text = "Not running in Work Profile.")
+            Text(text = "1. Setup work profile.")
+            Text(text = "2. Download the APK of app to be isolated.")
+            Text(text = "3. Pick the APK file to transfer to the work profile.")
+            Text(text = "4. Install the app in the work profile.")
+        }
+        if (isInstalledOnWorkProfile) {
+            if (vpnEnabled) {
+                Button(onClick = { disableVpn() }) {
+                    Text("Disable VPN")
+                }
+            } else {
+                Button(onClick = { enableVpn() }) {
+                    Text("Enable VPN")
+                }
+            }
+        } else {
+            Button(onClick = { setupWorkProfile() }) {
+                Text("Setup work profile")
+            }
+            Button(onClick = { pickFileToTransferToWorkProfile() }) {
+                Text("Pick file to be transferred")
             }
         }
 
